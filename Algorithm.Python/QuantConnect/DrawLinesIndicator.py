6d145c285f0bd4cfa43930bcc7a53e0d45db53c0
fanqineng <fanqineng@bytedance.com>
2019-12-30 15:13:14 +0800

    划线策略
    Change-Id: Ia2a45ff1406f8cb4e275cc02b7a45a0f59197c2e

--- Algorithm.Python/QuantConnect/DrawLinesIndicator.py
+++ Algorithm.Python/QuantConnect/DrawLinesIndicator.py
@@ -1,9 +1,67 @@
+from collection import deque
 from SarIndicator import SarIndicator
 
+class SarPoint(object):
+    def __init__(self, idx, value):
+        self.idx = idx
+        self.value = value
 
 class DrawLinesIndicator(object):
-    def __init__(self, ):
-        pass
+    def __init__(self, window=200, debug):
+        self.BarSeries = deque(maxlen=window)
+        self.Sar = SarIndicator()
+        self.Peaks = deque(maxlen=window)
+        self.Valleys = deque(maxlen=window)
+        self.Indices = deque(maxlen=window)
+        self.currentLineParams = dict(k=1, b=1) # 解析几何的直线公式
+        self.Value = None #
+        self.IsReady = False
+        self.Index = 0
+        self.Debug = debug
+
 
     def Update(self, bar):
-        pass
+        self.BarSeries.appendleft(bar)
+        self.Index += 1
+        self.Sar.Update(bar)
+        if not self.Sar.IsReady:
+            return
+
+        point_status = None
+        this_point = None
+        if self.Sar.CurrentColor == '红' and self.Sar.PreviousColor == "绿":
+            point_status = "Peak"
+            this_point = dict(x=self.Index, y=bar.Close)
+            self.Peaks.append(this_point)
+            if len(self.Peaks) > 3:
+                previous_base = self.Peaks[-3]
+        else if self.Sar.CurrentColor == '绿' and self.Sar.PreviousColor == "红":
+            point_status = "Valley"
+            this_point = dict(x=self.Index, y=bar.Close)
+            self.Valleys.append(this_point)
+
+        if point_status == "Peak" and len(self.Peaks) > 3 and len(self.Valleys) > 2 \
+            and self.Peaks[-3]['y'] > self.Peaks[-2]['y'] > self.Peaks[-1]['y'] and self.Peaks[-2]['y'] < self.Peaks[-1]['y']:
+            last3Peak = self.Peak[-3]
+            self.calLineParam(last3Peak['x'], last3Peak['y'], this_point['x'], this_point['y'])
+            self.Debug("Peak")
+        elif point_status == "Valley" and len(self.Valleys) > 3 and len(self.Peaks) > 2 \
+            and self.Valleys[-3]['y'] < self.Valleys[-2]['y'] < self.Valleys[-1]['y'] and self.Peaks[-2]['y'] < self.Peaks[-1]['y']:
+            last3Valley = self.Valley[-3]
+            self.calLineParam(last3Valley['x'], last3Valley['y'], this_point['x'], this_point['y'])
+            self.Debug("Valley")
+        else:
+            self.currentLineParams = None
+
+        if self.currentLineParams is not None:
+            self.IsReady = True
+
+    def CalPredicate(self, idx):
+        if self.currentLineParams:
+            return self.currentLineParams['k'] * idx + self.currentLineParams['b']
+        return None
+
+    def calLineParam(self, p1x, p1y, p2x, p2y):
+        k = (p2y - p1y) / (p2x - p1x)
+        b = (p1y*p2x - p2y*p1x) / (p2x - p1x)
+        self.currentLineParams = dict(k=k, b=b)
